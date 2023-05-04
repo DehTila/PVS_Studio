da8416ee4028d720d2e553f43a5e83798b751a46
DehTila <tilad@internet.ru>
2023-05-04 22:43:39 +0300

    Add file

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+int blin1(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+int blin2(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+int ppp()
+{
+    int pom;
+    std::cin >> pom;
+    if (pom = 3)
+    std::cout << "Не два =!";
+    else 
+        std::cout << "УВЫ";
+}
+int pop()
+{
+    if (1 > 0)
+        std::cout << "Опа";
+    else 
+        std::cout << "Опана";
+}
+int main()
+{
+    int a, b;
+    bool c;
+    std::cin >> a;
+    std::cin >> b;
+    if (blin1(0, 0) > 9)
+        c = false;
+    else
+        c = true;
+        c = false;
+        if (c == false)
+            std::cout << "ХАХАХАХАХа";
+        if (c == 9)
+            std::cout << "АХАХАХХАХА";
+}
